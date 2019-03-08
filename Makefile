97c1211cf8225dca072c7809104a5c6683c93a22
zhangtailin <zhangtailin@ziggurat.cn>
2019-03-08 10:37:57 +0800

    code clean

--- Makefile
+++ Makefile
@@ -6,13 +6,7 @@
 # This makefile defines the following targets
 #
 #   - protos - generate all protobuf artifacts based on .proto files
-#   - go-bindata - convert the Swagger UI to Go source code
 
 .PHONY: protos
 protos :
 	./scripts/compile_protos.sh
-
-.PHONY: go-bindata
-go-bindata :
-	./scripts/go-bindata.sh
-
--- scripts/compile_protos.sh
+++ scripts/compile_protos.sh
@@ -17,7 +17,7 @@ PROTO_ROOT_DIRS="$(dirname $PROTO_ROOT_FILES)"
 
 # As this is a proto root, and there may be subdirectories with protos, compile the protos for each sub-directory which contains them
 for protos in $(find "$PROTO_ROOT_DIRS" -name '*.proto' -exec dirname {} \; | sort | uniq) ; do
-    protoc --proto_path="$PROTO_ROOT_DIRS" -I./third_party/googleapis \
+    protoc --proto_path="$PROTO_ROOT_DIRS" \
             --go_out=plugins=grpc:$GOPATH/src \
             "$protos"/*.proto
 done
